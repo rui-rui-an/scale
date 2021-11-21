090cc02747837e126ffeeef83011635921f8623a
rui-rui-an <1405613785@qq.com>
2021-11-21 16:41:15 +0800

    链接github

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <div class="mybox" :style="{'--bgc':bgColor}">我盒子</div>
+    <el-button @click="changeColor">改变背景颜色</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      bgColor:'red'
+    }
+  },
+  methods: {
+    changeColor () {
+      this.bgColor = 'blue'
+      console.log(111);
+    },
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.home {
+  .mybox {
+    width: 50px;
+    height: 50px;
+    background-color: var(--bgc);
+  }
+}
+</style>
