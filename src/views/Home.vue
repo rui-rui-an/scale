a54ad70c9bb87deb33a6e3db6be9b9ca9c71a68b
rui-rui-an <1405613785@qq.com>
2021-12-19 23:28:52 +0800

    不需要的代码删除

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,53 @@
 <template>
   <div class="home">
-    <div class="mybox" :style="{'--bgc':bgColor}">我盒子</div>
-    <el-button @click="changeColor">改变背景颜色</el-button>
+    <div class="mybox" :style="{ '--bgc': bgColor }">我盒子</div>
+    <el-select v-model="value" @change="changeColor" placeholder="请选择">
+      <el-option
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+      >
+      </el-option>
+    </el-select>
   </div>
 </template>
 
 <script>
 export default {
-  data(){
-    return{
-      bgColor:'red'
+  data () {
+    return {
+      bgColor: 'red',
+      options: [
+        {
+          value: '#0094ff',
+          label: '蓝色'
+        },
+        {
+          value: '#f9d20f',
+          label: '金色'
+        },
+        {
+          value: 'black',
+          label: '黑色'
+        },
+        {
+          value: 'green',
+          label: '绿色'
+        },
+        {
+          value: 'red',
+          label: '红色'
+        }
+      ],
+      value: ''
     }
   },
   methods: {
-    changeColor () {
-      this.bgColor = 'blue'
-      console.log(111);
-    },
+    changeColor (val) {
+      // console.log(val)
+      this.bgColor = val
+    }
   }
 }
 </script>
