f98cc0b62736c117bf79997679a65759136cdcb2
rui-rui-an <1405613785@qq.com>
2021-12-26 00:42:24 +0800

    完成自定义颜色的配置

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,40 @@
 <template>
   <div class="home">
-    <div class="mybox" :style="{ '--bgc': bgColor }">我盒子</div>
-    <div class="change_box_color">
-      <div>改变盒子颜色</div>
-      <el-select v-model="value" @change="changeColor" placeholder="请选择">
-        <el-option
-          v-for="item in options"
-          :key="item.value"
-          :label="item.label"
-          :value="item.value"
+    
+    <div class="left">
+      <div class="change_box_color">
+        <div>改变盒子颜色：</div>
+        <el-select v-model="value" @change="changeColor" placeholder="请选择">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          >
+          </el-option>
+        </el-select>
+      </div>
+      <div class="change_box_color changbg">
+        <div>改变背景颜色：</div>
+        <div
+          class="choose_color"
+          :style="{ backgroundColor: colors }"
+          @click.stop="showChooseColor"
         >
-        </el-option>
-      </el-select>
+          <div v-if="showChoose" class="color_picker_box">
+            <photoshop-picker
+              :value="OldColor"
+              @input="updateValue"
+              @ok="onOk"
+              @cancel="onCancel"
+            ></photoshop-picker>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="change_box_color">
-      <div>改变背景颜色</div>
-      <div
-        class="choose_color"
-        :style="{'backgroundColor':colors}"
-      ></div>
-      <!-- <photoshop-picker v-model="colors"></photoshop-picker> -->
+    <div class="right">
+      <div class="mybox" :style="{ '--bgc': bgColor }">我盒子</div>
+      <div class="others" :style="{ backgroundColor: colors }"></div>
     </div>
   </div>
 </template>
@@ -33,7 +48,9 @@ export default {
   data() {
     return {
       colors: "#0094ff",
-      bgColor: "red",
+      OldColor: "#0094ff",
+      bgColor: "#97a5b1",
+      showChoose: false,
       options: [
         {
           value: "#0094ff",
@@ -61,8 +78,24 @@ export default {
   },
   methods: {
     changeColor(val) {
-      // console.log(val)
       this.bgColor = val
+    },
+    showChooseColor(e) {
+      const clickDiv = document.querySelector(".choose_color")
+      if (e.target === clickDiv) {
+        this.showChoose = !this.showChoose
+      }
+    },
+    updateValue(c) {
+      this.colors = c.hex
+    },
+    onOk() {
+      this.OldColor = this.colors
+      this.showChoose = false
+    },
+    onCancel() {
+      this.colors = this.OldColor
+      this.showChoose = false
     },
   },
 }
@@ -70,23 +103,40 @@ export default {
 
 <style lang="less" scoped>
 .home {
-  // background-color: #0094ff;
-  height: 80vh;
+  display: flex;
+  .left {
+    width: 350px;
+    
+  }
+  .right {
+      flex: 1;
+  }
   .mybox {
-    width: 50px;
+    width: 100%;
     height: 50px;
     background-color: var(--bgc);
   }
   .change_box_color {
     display: flex;
     align-items: center;
-    justify-content: center;
     .choose_color {
       width: 40px;
       height: 40px;
-      // background-color: red;
+      position: relative;
       cursor: pointer;
+      .color_picker_box {
+        position: absolute;
+        top: 40px;
+        left: 0;
+      }
     }
+  }
+  .changbg {
+    margin-top: 20px;
+  }
+  .others {
+    height: 76vh;
+    margin-top: 20px;
   }
 }
 </style>
